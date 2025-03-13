34488f5fa299460f86c084cb0e2ad8e0f00fb9b1
WQJ <wqjuser@gmail.com>
2025-03-14 00:18:17 +0800

    build(config): 更新软件版本号
    - 将软件版本号从 0.3.9 修改为 0.3.9.1

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ class Config:
 
         self._initialized = True
         
-        self.version = "0.3.9"
+        self.version = "0.3.9.1"
 
         # 获取应用程序的根目录路径
         if getattr(sys, "frozen", False):
